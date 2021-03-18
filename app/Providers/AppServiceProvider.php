6f045d8ae5ac4ab9a41aae56227467af74b9cbee
Iva <lllllexxxxx@gmail.com>
2021-03-18 18:14:39 +0200

    sql too long

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //URL::forceScheme('https');
+        Schema::defaultStringLength(191);
     }
 
     /**
